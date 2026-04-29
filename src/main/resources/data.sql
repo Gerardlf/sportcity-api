93e42f6e419a8759e05392efc88b64fbda58fa81
Gerardo de la fuente <gerdeluri@alu.edu.gva.es>
2026-04-29 12:27:55 +0200

    properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,23 @@
-INSERT INTO pistas (nombre, deporte, precio_hora, descripcion) VALUES ('Pista Central', 'Tenis', 15.0, 'Pista rápida de tenis');
-INSERT INTO pistas (nombre, deporte, precio_hora, descripcion) VALUES ('Campo F7 Norte', 'Fútbol 7', 30.0, 'Césped artificial de última generación');
-INSERT INTO pistas (nombre, deporte, precio_hora, descripcion) VALUES ('Pista Pádel 1', 'Pádel', 18.0, 'Pista de cristal');
-INSERT INTO pistas (nombre, deporte, precio_hora, descripcion) VALUES ('Pista Basket', 'Basket', 20.0, 'Pista cubierta');
+INSERT INTO pistas (nombre, deporte, precio_hora, descripcion)
+SELECT 'Pista Central', 'Tenis', 15.0, 'Pista rapida de tenis'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM pistas WHERE nombre = 'Pista Central'
+);
+
+INSERT INTO pistas (nombre, deporte, precio_hora, descripcion)
+SELECT 'Campo F7 Norte', 'Futbol 7', 30.0, 'Cesped artificial de ultima generacion'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM pistas WHERE nombre = 'Campo F7 Norte'
+);
+
+INSERT INTO pistas (nombre, deporte, precio_hora, descripcion)
+SELECT 'Pista Padel 1', 'Padel', 18.0, 'Pista de cristal'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM pistas WHERE nombre = 'Pista Padel 1'
+);
+
+INSERT INTO pistas (nombre, deporte, precio_hora, descripcion)
+SELECT 'Pista Basket', 'Basket', 20.0, 'Pista cubierta'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM pistas WHERE nombre = 'Pista Basket'
+);
